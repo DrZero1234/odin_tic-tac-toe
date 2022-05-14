1b4d4040b11d48735792b4acad5517e5bdc04cf8
M.Kristof <drzero123456@gmail.com>
2022-05-14 22:40:48 +0200

    pseudo code added

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("testing")
+const gameBoard = (() => {
+    /*
+        SET UP GAMEBOARD CONTET
+        A function which will store the gamebaord in an array
+        and generate it to the website filling it with "X" or "O"-s 
+    */
+
+    /*
+        PLAYER MARKS
+        A function which will be created to handle the logic of players playing their move
+        on the field
+    */
+
+    /* POST GAME
+        A function which will handle the end of the game which should include 
+        + Cleaning up the interface
+        + Congratulating the winner or the notify if the round was draw
+        + Button to restart the game
+    */
+})()
+
+const Player = (name,mark) => {
+    name = obj.name;
+    mark = obj.makr
+}
+
+const playGame = (() => {
+    /*
+     GAME LOGIC
+     A function which will check if 3 of the same marks match up
+     vertically, horizontally or across
+    */
+
+})()
