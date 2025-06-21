642c271cf6dadd4e3487ba706e16b0a760e43433
Drzero1234 <drzero123456@gmail.com>
2025-06-21 13:53:23 +0200

    Format change

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-const GAMEBOARD_DIV = document.getElementById("gameboard")
+const GAMEBOARD_DIV = document.getElementById("gameboard");
 
 const gameboard = (() => {
-    board = []
-    const getBoard = () => {
-        return board
-    }
+  board = [];
+  const getBoard = () => {
+    return board;
+  };
 
+  return { getBoard };
+})();
 
-    return {getBoard}
-})()
-
-const Player  = (name, mark) => {
-
-    return {name, mark}
-}
-
+const Player = (name, mark) => {
+  return { name, mark };
+};
 
 const generate_board = (() => {
-    board = gameboard.getBoard();
-    for (let i = 0; i < 9;i++) {
-        const div_elem = document.createElement("div")
-        div_elem.className = "cell";
-        div_elem.setAttribute("data-index", i);
-        div_elem.textContent = "";
-        GAMEBOARD_DIV.appendChild(div_elem);
-        board.push("")
+  board = gameboard.getBoard();
+  for (let i = 0; i < 9; i++) {
+    const div_elem = document.createElement("div");
+    div_elem.className = "cell";
+    div_elem.setAttribute("data-index", i);
+    GAMEBOARD_DIV.appendChild(div_elem);
+    board.push("");
+  }
+})();
+
+const displayController = () => {
+  const createPlayer = (mark) => {
+    if (mark.toLowerCase() === "X".toLowerCase()) {
+      player1 = Player("Player1", "X");
+      player2 = Player("Player2", "O");
+    } else if (mark.toLowerCase() === "O".toLowerCase()) {
+      player1 = Player("Player1", "O");
+      player2 = Player("Player2", "X");
     }
-})()
+    return { player1, player2 };
+  };
 
+  const toggleClass = (hide, appear) => {
+    hide.className = "disabled";
+    appear.className = "";
+  };
 
-const displayController =  () => {
-
-    const createPlayer = (mark) => {
-        if (mark === "X") {
-            player1 = Player("Player1", "X");
-            player2 = Player("Player2", "O");
-        } else {
-            player1 = Player("Player1", "O");
-            player2 = Player("Player2", "X")
-        }
-        return {player1, player2}
-    }
-
-    const toggleClass = (hide, appear) => {
-        hide.className = "disabled";
-        appear.className = ""
-    }
-
-    return { createPlayer}
-}
+  return { createPlayer };
+};
 
 const playGame = (() => {
-    const MARK_BTNS = document.querySelector(".mark-select").querySelectorAll("button")
-    MARK_BTNS.forEach((button) => {
-        button.onclick = displayController.createPlayer(button.textContent)
-    })
-})()
+  const MARK_BTNS = document
+    .querySelector(".mark-select")
+    .querySelectorAll("button");
+  MARK_BTNS.forEach((button) => {
+    button.onclick = displayController.createPlayer(button.textContent);
+  });
+})();
